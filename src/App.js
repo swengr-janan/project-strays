d99399b58afc743008251158c18fabc0743333bc
Justin <justinanthony.primary@gmail.com>
2022-08-07 09:54:40 +0800

    feat: add minting soon page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
+// import Navbar from './components/Navbar'
+import { MintingSoon } from './pages'
 import './App.css'
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" exact />
-      </Routes>
-    </Router>
+const App = () => (
+  <Router>
+    {/* <Navbar /> */}
+    <MintingSoon />
+    <Routes>
+      <Route path="/" exact />
+    </Routes>
+  </Router>
 
-  )
-}
+)
 
 export default App
